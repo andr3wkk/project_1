6834abd1f8f31768cd86e21766ba671c9796e837
andrewkovalenko <andrewkovalenko2005@icloud.com>
2023-08-24 21:33:17 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+        System.out.println("My name is Andrii Kovalenko, studying for BSE. Welcome to my first Java program!");
+    }
+}
